2ed025bac7a99ff5b70649aa96f4be30cc540825
Shane Hagan <49661276+HaganShane@users.noreply.github.com>
2022-01-26 11:04:49 -0500

    Create FirstReverse.py

---
+++ Coderbyte/First Reverse/FirstReverse.py
@@ -0,0 +1,11 @@
+# Author: Shane Hagan
+# Date: 01/26/2022
+# Purpose: practice coding with python on Coderbyte
+
+def FirstReverse(strParam):
+
+  # Easiest way to splice the string and return it. Start / stop and then -1 to reverse
+  return strParam[::-1]
+
+# keep this function call here 
+print(FirstReverse(input()))
